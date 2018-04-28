aa2754f1025b84374d1e70fff0d87f778e03d56a
Suman Barick <codotronix@gmail.com>
2018-04-28 15:54:15 +0530

    under dev banner fixed in mob

--- www/dist/scripts/properties.js
+++ www/dist/scripts/properties.js
@@ -12,6 +12,9 @@
         "config": {
             "homeConfig": "configs/homeConfig.json"
         },
+
+        "isDevInProgress": true,
+        "devInProgressMsg": 'This site is currently under development. Join discussions at <a href="https://www.facebook.com/knexaa"><u>facebook.com/knexaa</u></a>',
         
         "uniKey": "7744863-14135635-13322038-2015080-16136753-19833163-12514593-17757518-4579128-20503651-7431460-6726711-16517190-5287019-14555213-6265901-17583703-4939299-14636857-8574514-19331705-23233876-11785833-4673144-13136164-10791488-16075864-24241465-11007347-23276122"
 	};
